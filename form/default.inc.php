24d13f86d4a60d79650e027af77db5be9f8c7fd3
Katie Lynn Swiatek <klswiatek@yahoo.com>
2021-08-03 23:16:05 -0400

    All files added to project folder.

---
+++ form/default.inc.php
@@ -0,0 +1,37 @@
+<style type="text/css">
+* { margin: 0;
+padding: 0;
+border: 0;
+}
+body {
+    background-image: url(../image/watercolor.jpeg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+  }
+.navbar {
+    background-color: lightpink;
+    text-align: center;
+    color: white;
+    font-family: sans-serif;
+    font-size: 20px;
+    padding: 15px;
+    max-width: 775px;
+    margin: auto;
+  }
+  .navbar a:hover {
+    background-color: #ddd;
+    color: lightpink;
+  }
+</style>
+<header>
+  <div class="navbar">
+    <a class="active" href="">Home</a>
+    <a href="">About</a>
+    <a href="">Graphic Design</a>
+    <a href="">Photography</a>
+  </div>
+</header>
+<div class="container">
+  <h1>Sorry, something went wrong! Please go back and try again.</h1>
+</div>
---
+++ form/thankyou.inc.php
@@ -0,0 +1,71 @@
+<style type="text/css">
+* { margin: 0;
+padding: 0;
+border: 0;
+}
+body {
+    background-color: lightblue;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+  }
+p {
+  display: block;
+  padding: 7px;
+  font-size: 20px;
+  text-align: center;
+  font-family: monospace;
+}
+p::first-line {
+    color: white;
+  }
+header .navbar {
+    background-color: lightpink;
+    text-align: center;
+    color: white;
+    font-family: sans-serif;
+    font-size: 20px;
+    padding: 15px;
+    max-width: 775px;
+    margin: auto;
+  }
+.navbar {
+      background-color: lightpink;
+      text-align: center;
+      color: white;
+      font-family: sans-serif;
+      font-size: 20px;
+      padding: 15px;
+      max-width: 775px;
+      margin: auto;
+    }
+.navbar a:hover {
+    background-color: #ddd;
+    color: lightpink;
+  }
+h1 {
+    display: block;
+    padding: 7px;
+    font-size: 18px;
+     margin: auto;
+     text-align: center;
+     font-family: monospace;
+     font-weight: bolder;
+  }
+a:link, a:visited {
+    color: black;
+  }
+</style>
+<header>
+  <div class="navbar">
+    <a class="active" href="/../index.html">Home</a>
+    <a href="">About</a>
+    <a href="">Graphic Design</a>
+    <a href="">Photography</a>
+  </div>
+</header>
+<body>
+<h1>Thank you for your message!</h1>
+<p>I have received your message, and we will be in touch shortly.</p>
+<p>Form processor provided by <a href="http://www.inventpartners.com">Invent Partners web design York</a>.</p>
+</body>
